f63c94b5e61855cc8b1bbcc4d70de54b213e4cff
1293433409@qq.com <123456>
2017-03-16 18:55:29 +0800

    .

--- app/src/main/java/com/k/baselayout/MainActivity.java
+++ app/src/main/java/com/k/baselayout/MainActivity.java
@@ -2,10 +2,13 @@ package com.k.baselayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "调用sdk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    // 调用流程
     private void test() {
+        BaseSDK.checkLiveness("openID", new BaseSDKCallback() {
+            @Override
+            public void onCheckResult(boolean success, String token) {
+                if (success) {
+                    Log.i(TAG, "验证成功, token为:" + token);
+                } else {
+                    Log.i(TAG, "验证不通过");
+                }
+            }
 
-
+            @Override
+            public void onCheckFail(ZTOFaceLivenessError error) {
+                switch (error) {
+                    case ZTOFaceLivenessCancel:
+                    {
+                        Log.i(TAG, "用户主动取消");
+                    }
+                    break;
+                    case ZTOFaceLivenessFail:
+                    {
+                        Log.i(TAG, "网络请求失败?");
+                    }
+                    break;
+                    case ZTOFaceLivenessForbidden:
+                    {
+                        Log.i(TAG, "权限禁止");
+                    }
+                    break;
+                }
+            }
+        });
     }
 }
